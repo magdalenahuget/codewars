5ff671bad33e1826b9d9ee011d5bf9afdc446cec
magdalenahuget <magdalena.huget@gmail.com>
2022-11-19 14:34:05 +0100

    Add count of positives sum of negatives

---
+++ javascript/8kyu/countOfPositivesSumOfNegatives.js
@@ -0,0 +1,18 @@
+let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+function countPositivesSumNegatives(input) {
+    let sumPositive = 0;
+    let sumNegative = 0;
+    let result = [];
+    for (oneIndex of input) {
+      if (oneIndex > 0) {
+        sumPositive += 1;
+      } else if (oneIndex < 0){
+        sumNegative += oneIndex;
+      } 
+      
+    }
+    result.push(sumPositive)
+    result.push(sumNegative)
+    return result
+  }
+  console.log(countPositivesSumNegatives(input))
