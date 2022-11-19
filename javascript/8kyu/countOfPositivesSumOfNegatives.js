ee08dbff6e59f566b69a83efa8a1fa38c0fd29bf
magdalenahuget <magdalena.huget@gmail.com>
2022-11-19 15:52:26 +0100

    Add cound positives sum negatives.

--- javascript/8kyu/countOfPositivesSumOfNegatives.js
+++ javascript/8kyu/countOfPositivesSumOfNegatives.js
@@ -1,18 +1,19 @@
-let input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
 function countPositivesSumNegatives(input) {
     let sumPositive = 0;
     let sumNegative = 0;
     let result = [];
+    if (!input || input.length === 0) {
+        return result;
+    }
     for (let oneIndex of input) {
-      if (oneIndex > 0) {
-        sumPositive += 1;
-      } else if (oneIndex < 0){
-        sumNegative += oneIndex;
-      } 
-      
+        if (oneIndex > 0) {
+            sumPositive += 1;
+        } else if (oneIndex < 0) {
+            sumNegative += oneIndex;
+        }
+
     }
     result.push(sumPositive)
     result.push(sumNegative)
     return result
-  }
-  console.log(countPositivesSumNegatives(input))
+}
