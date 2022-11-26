3708e503b30dea39e2bad0af7a39ff3511502d5d
magdalenahuget <magdalena.huget@gmail.com>
2022-11-26 12:26:54 +0100

    Fix Nth power.

--- javascript/8kyu/Nthpower.js
+++ javascript/8kyu/Nthpower.js
@@ -1,14 +1,12 @@
 function index(array, n) {
     let result = 0;
     let npow = 0;
-    for (let i = 0; i < array.length; i++) {
       if (n <= array.length){
           npow = Math.pow(array[n], n);
           result = npow;
         } else {
           result = -1
         }
-    }
     return result;
 }
 
