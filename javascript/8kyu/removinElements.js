0eaa63a415c6dabff330f2ce4c4f49a5bff10f60
magdalenahuget <magdalena.huget@gmail.com>
2022-12-02 17:02:14 +0100

    Add removing elements

---
+++ javascript/8kyu/removinElements.js
@@ -0,0 +1,11 @@
+//Removing elements from array
+
+function removeEveryOther(arr){
+    let result = [];
+        for(let i=0; i < arr.length; i++){
+           if(i % 2 === 0){
+            result.push(arr[i]);
+           }
+        }
+          return result;
+  }
