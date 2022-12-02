d20336995360c96a4b3fa872d3d28f77aa85d451
magdalenahuget <magdalena.huget@gmail.com>
2022-12-02 17:50:16 +0100

    Add I love you, alittle, a lot...

---
+++ javascript/8kyu/iLoveYou,alittle.js
@@ -0,0 +1,24 @@
+function howMuchILoveYou(nbPetals) {
+    let result = "";
+    for (let i = 0; i < nbPetals; i++) {
+        if (nbPetals === 7) {
+            result = "I love you"
+        }
+        if (nbPetals === 2) {
+            result = "a little"
+        }
+        if (nbPetals === 3) {
+            result = "a lot"
+        }
+        if (nbPetals === 4) {
+            result = "passionately"
+        }
+        if (nbPetals === 5) {
+            result = "madly"
+        }
+        if (nbPetals === 6) {
+            result = "not at all"
+        }
+    }
+    return result;
+}
