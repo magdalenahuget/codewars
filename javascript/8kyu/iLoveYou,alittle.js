bf5083ea822396dc11e5b886b3aad5a5b83b6a55
magdalenahuget <magdalena.huget@gmail.com>
2022-12-02 18:02:26 +0100

    Add second solution  I love you, alittle, a lot...

--- javascript/8kyu/iLoveYou,alittle.js
+++ javascript/8kyu/iLoveYou,alittle.js
@@ -22,3 +22,13 @@ function howMuchILoveYou(nbPetals) {
     }
     return result;
 }
+
+// second version
+
+function howMuchILoveYou(nbPetals) {
+    let arr = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
+    for (let i = 0; i <= nbPetals; i++){
+    arr.push(arr[i]);
+    }
+    return arr[nbPetals-1];
+   }
