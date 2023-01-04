dc32044ceb02018015350f566224f907834dad22
magdalenahuget <magdalena.huget@gmail.com>
2023-01-04 13:53:19 +0100

    Refactor

--- javascript/8kyu/arraDiff.js
+++ javascript/8kyu/arraDiff.js
@@ -12,3 +12,20 @@ function arrayDiff(a, b) {
 
 
   // Jak usunąć 'undefined' z naszego array ===>>>> array.filter(Boolean)
+
+
+  // version 2 - working better
+  function arrayDiff(a, b) {  
+    if (b.length == 0 || a.length == 0) {
+      return a;
+    }
+  for (var j = 0; j < a.length; j++) {
+    for (var i = 0; i < b.length; i++) {
+      if(a[j] == b[i]) {
+        a.splice(j, 1);
+        j--;
+        }
+      }
+    }
+    return a;
+  }
